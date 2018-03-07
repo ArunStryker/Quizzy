5a21e6106886718cbd805485ef30449b4159f1cc
Arun Venkatesh <arun070996@gmail.com>
2018-03-07 18:30:16 +0530

    Removed Unnecessary Comments

--- app/src/main/java/com/example/arunvenkatesh/quizzy/MainActivity.java
+++ app/src/main/java/com/example/arunvenkatesh/quizzy/MainActivity.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 
-
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
